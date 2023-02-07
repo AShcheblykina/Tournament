da2963bc4ec1ea2b06af3f409353fa946d1c60ba
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-02-07 21:01:54 +0300

    changed project

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -9,55 +9,8 @@ public class GameTest {
     public void winFirstPlayer() throws NotRegisteredException {
         Game game = new Game();
 
-        Player player1 = new Player(1,"Koly",50);
-        Player player2 = new Player(2,"Any", 80);
-
-        game.register(player1);
-        game.register(player2);
-
-        int actual = game.round("Koly","Any");
-        int expected = 2;
-
-        Assertions.assertEquals(expected,actual);
-
-    }
-    @Test
-    public void winTwoPlayer() {
-        Game game = new Game();
-
-        Player player1 = new Player(1,"Koly",100);
-        Player player2 = new Player(2,"Any", 80);
-
-        game.register(player1);
-        game.register(player2);
-
-        int actual = game.round("Koly","Any");
-        int expected = 1;
-
-        Assertions.assertEquals(expected,actual);
-
-    }
-    @Test
-    public void winDraw() {
-        Game game = new Game();
-
-        Player player1 = new Player(1,"Koly",100);
-        Player player2 = new Player(2,"Any", 100);
-
-        game.register(player1);
-        game.register(player2);
-
-        int actual = game.round("Koly","Any");
-        int expected = 0;
-
-        Assertions.assertEquals(expected,actual);
-    }
-    @Test
-    public void notRegister() {
-        Game game = new Game();
-
-        Player player1 = new Player(1, "Koly", 100);
-        Player player2 = new Player(2, "Any", 60);
+        Player player1 = new Player(1, "Koly", 50);
+        Player player2 = new Player(2, "Any", 80);
 
         game.register(player1);
         game.register(player2);
@@ -65,29 +18,73 @@ public class GameTest {
         int actual = game.round("Koly", "Any");
         int expected = 2;
 
-        Assertions.assertThrows(NotRegisteredException.class, () -> {
-            ArrayList.round(player1);
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void winTwoPlayer() {
+        Game game = new Game();
+
+        Player player1 = new Player(1, "Koly", 100);
+        Player player2 = new Player(2, "Any", 80);
+
+        game.register(player1);
+        game.register(player2);
+
+        int actual = game.round("Koly", "Any");
+        int expected = 1;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void winDraw() {
+        Game game = new Game();
+
+        Player player1 = new Player(1, "Koly", 100);
+        Player player2 = new Player(2, "Any", 100);
+
+        game.register(player1);
+        game.register(player2);
+
+        int actual = game.round("Koly", "Any");
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void notRegister() {
+        Game game = new Game();
+
+        Player player1 = new Player(1, "Koly", 100);
+        Player player2 = new Player(2, "Any", 60);
+
+        game.register(player2);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            game.round("Koly", "Any");
         });
     }
+
     @Test
     public void notRegisterTo() {
         Game game = new Game();
 
-        Player player1 = new Player(1,"Koly",100);
-        Player player2 = new Player(2,"Any", 60);
+        Player player1 = new Player(1, "Koly", 100);
+        Player player2 = new Player(2, "Any", 60);
 
-        //game.register(player1);
-        game.register(player2);
+        game.register(player1);
 
-        int actual = game.round("Koly","Any");
-        int expected = 2;
+        int expected = 1;
 
-        Assertions.assertEquals(expected,actual);
-
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            game.round("Koly", "Any");
+        });
 
     }
-
-
 
 
 }
